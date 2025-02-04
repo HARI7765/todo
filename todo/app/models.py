de90dd22e2ece6bf40fb7c7dd265cba7bf37aeae
harikrishna0001 <nharikrishnanm@gmail.com>
2025-02-04 18:27:06 +0530

    updated

--- todo/app/models.py
+++ todo/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    # Name field (string)
+    name = models.CharField(max_length=255)
+    
+    # Email field (email type)
+    email = models.EmailField(unique=True)
+    
+    # Phone number field (integer or string-based)
+    phone = models.CharField(max_length=15)  # using CharField for flexibility
+    
+    # Date of birth field
+    dob = models.DateField()
+
+    def __str__(self):
+        return self.name
--- todo/app/views.py
+++ todo/app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
+def main(request):
+    if request.POST:
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        obj1=UserProfile(name=name,email=email)
+        obj1.save()
+    return render(request,"main.html")
