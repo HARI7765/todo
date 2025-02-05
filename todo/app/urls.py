3428e87d2d062968c27d39a28f08178c80ca669b
harikrishna0001 <nharikrishnanm@gmail.com>
2025-02-05 15:42:13 +0530

    completed

--- todo/app/urls.py
+++ todo/app/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.main,name="main"),
+    path('delete_user/<int:user_id>/', views.delete_user, name='delete_user'),  # Delete user URL
+
    
     
 ]
--- todo/app/views.py
+++ todo/app/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import *
 
 # Create your views here.
 def main(request):
     if request.POST:
-        name=request.POST.get('name')
-        email=request.POST.get('email')
-        obj1=UserProfile(name=name,email=email)
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        dob = request.POST.get('dob')
+        obj1 = UserProfile(name=name, email=email, phone=phone, dob=dob)
         obj1.save()
-    return render(request,"main.html")
+
+    # Fetch all UserProfile objects
+    obj = UserProfile.objects.all()
+
+    # Correct the context: pass a dictionary
+    return render(request, "main.html", {"det": obj})
+
+def delete_user(request, user_id):
+    # Fetch the user object using the id passed from the URL
+    user_to_delete = get_object_or_404(UserProfile, id=user_id)
+    
+    # Delete the object
+    user_to_delete.delete()
+
+    # Redirect back to the main page after deletion
+    return redirect('main')  # Make sure 'main' is the name of the URL fo
